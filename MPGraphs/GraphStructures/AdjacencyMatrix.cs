7719e3b7ebb4ae7ee6924f231d95260bcf11e280
Michał Piekarski <michal.piekarski@icloud.com>
2015-12-04 00:37:38 +0100

    Comments duplicated in grap representaion class stubs

--- MPGraphs/GraphStructures/AdjacencyMatrix.cs
+++ MPGraphs/GraphStructures/AdjacencyMatrix.cs
@@ -8,26 +8,57 @@ namespace MPGraphs.GraphStructures
 {
     class AdjacencyMatrix<T> : Matrix<T>, IGraphRepresentation
     {
+        /// <summary>
+        /// Adds an edge to the graph representation, between two vertices at indexes: <paramref name="vertexIndexA"/> and <paramref name="vertexIndexB"/>.
+        /// </summary>
+        /// <param name="vertexIndexA">First vertex of the edge.</param>
+        /// <param name="vertexIndexB">Second vertex of the edge.</param>
         public void AddEdge(int vertexIndexA, int vertexIndexB)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Adds new vertex to graph representation.
+        /// </summary>
         public void AddVertex()
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Checks if the edge between two specified vertices at indexes: <paramref name="vertexIndexA"/> and <paramref name="vertexIndexB"/> exists.
+        /// </summary>
+        /// <param name="vertexIndexA">First vertex of the edge.</param>
+        /// <param name="vertexIndexB">Second vertex of the edge to find.</param>
+        /// <returns>
+        /// If the edge between specified vertices doesn't exist, returns <c>false</c> (otherwise returns <c>true</c>).
+        /// </returns>
         public bool FindEdge(int vertexIndexA, int vertexIndexB)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Removes an edge from the graph representation, between two vertices at indexes: <paramref name="vertexIndexA"/> and <paramref name="vertexIndexB"/> if it exists.
+        /// </summary>
+        /// <param name="vertexIndexA">First vertex of the edge.</param>
+        /// <param name="vertexIndexB">Second vertex of the edge.</param>
+        /// <returns>
+        /// If the edge between two specified vertices doesn't exist, return <c>false</c> (otherwise returns <c>true</c>).
+        /// </returns>
         public bool RemoveEdge(int vertexIndexA, int vertexIndexB)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Removes a specified vertex at index == <paramref name="vertexIndex"/> from graph representation if it exists.
+        /// </summary>
+        /// <param name="vertexIndex">Index of the vertex to remove from graph representation.</param>
+        /// <returns>
+        /// If the vertex at index == <paramref name="vertexIndex"/> doesn't exist, return <c>false</c> (otherwise return <c>true</c>).
+        /// </returns>
         public bool RemoveVertex(int vertexIndex)
         {
             throw new NotImplementedException();
