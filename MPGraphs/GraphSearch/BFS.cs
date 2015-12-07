d713b4d1e7eb9b44c5c3572fe8d0d1b40e62a48c
Michał Piekarski <michal.piekarski@icloud.com>
2015-12-07 19:30:18 +0100

    Added BFS graph search class and matching unit tests

---
+++ MPGraphs/GraphSearch/BFS.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MPGraphs.GraphStructures;
+
+namespace MPGraphs.GraphSearch
+{
+    public class BFS<T> : IDisposable where T : class, IGraphRepresentation, new()
+    {
+        private List<int> Numer;
+        private int Ponumerowano;
+        private Queue<int> Kolejka;
+        private T Drzewo;
+        private T Pozostałe;
+        public BFS()
+        {
+            this.Numer = new List<int>();
+            this.Ponumerowano = 0;
+            this.Kolejka = new Queue<int>();
+            this.Drzewo = new T();
+            this.Pozostałe = new T();
+        }
+        public void Dispose()
+        {
+        }
+
+        public Tuple<List<int>, T, T> Search(T G, int x)
+        {
+            for (int i = 0; i < G.VertexCount; i++)
+            {
+                this.Numer.Add(0);
+                this.Drzewo.AddVertex();
+                this.Pozostałe.AddVertex();
+            }
+            Numer[x] = 1;
+            Ponumerowano = 1;
+            Kolejka.Enqueue(x);
+            while (Kolejka.Count > 0)
+            {
+                int v = Kolejka.Dequeue();
+                List<int> Iv = G.FindAdjacentEdges(v);
+                for (int i = 0; i < Iv.Count; i++)
+                {
+                    int w = Iv[i];
+                    if (Numer[w] == 0)
+                    {
+                        Ponumerowano = Ponumerowano + 1;
+                        Numer[w] = Ponumerowano;
+                        Drzewo.AddEdge(v, w);
+                        Kolejka.Enqueue(w);
+                    }
+                    else { Pozostałe.AddEdge(v, w); }
+                }
+            }
+            return new Tuple<List<int>, T, T>(Numer, Drzewo, Pozostałe);
+        }
+        public void Clear()
+        {
+            this.Numer = new List<int>();
+            this.Ponumerowano = 0;
+            this.Kolejka = new Queue<int>();
+            this.Drzewo = new T();
+            this.Pozostałe = new T();
+        }
+    }
+}
---
+++ MPGraphsTests/GraphSearch/BFSTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPGraphs.GraphSearch;
+using MPGraphs.GraphStructures;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPGraphs.GraphSearch.Tests
+{
+    [TestClass()]
+    public class BFSTests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            AdjacencyMatrix m = new AdjacencyMatrix();
+            for (int i = 0; i < 7; i++)
+            {
+                m.AddVertex();
+            }
+            m.AddEdge(0, 1);
+            m.AddEdge(0, 4);
+            m.AddEdge(1, 2);
+            m.AddEdge(1, 4);
+            m.AddEdge(1, 5);
+            m.AddEdge(5, 6);
+            m.AddEdge(2, 3);
+            Assert.IsTrue(m.EdgeCount == 7);
+            using (BFS<AdjacencyMatrix> bfs = new BFS<AdjacencyMatrix>())
+            {
+                Tuple<List<int>, AdjacencyMatrix, AdjacencyMatrix> bfsResult = bfs.Search(m, 0);
+                List<int> expectedResult = new List<int>(new int[] { 1, 2, 4, 6, 3, 5, 7 });
+                CollectionAssert.AreEqual(expectedResult, bfsResult.Item1);
+                CollectionAssert.DoesNotContain(bfsResult.Item1, 0);
+                Assert.IsTrue(bfsResult.Item2.EdgeCount == 6);
+
+                bfs.Clear();
+
+                bfsResult = bfs.Search(m, 1);
+                expectedResult = new List<int>(new int[] { 2, 1, 3, 6, 4, 5, 7 });
+                CollectionAssert.AreEqual(expectedResult, bfsResult.Item1);
+                CollectionAssert.DoesNotContain(bfsResult.Item1, 0);
+                Assert.IsTrue(bfsResult.Item2.EdgeCount == 6);
+            }
+        }
+    }
+}
