8236ce422b993c20ab276a23813e14142d961807
Michał Piekarski <michal.piekarski@icloud.com>
2015-12-07 20:38:03 +0100

    Added DFS search class and its unit tests

---
+++ MPGraphs/GraphSearch/DFS.cs
@@ -0,0 +1,74 @@
+﻿using MPGraphs.GraphStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPGraphs.GraphSearch
+{
+    public class DFS<T> : IGraphSearch<T>, IDisposable where T : class, IGraphRepresentation, new()
+    {
+        private List<int> Numer;
+        private int Ponumerowano;
+        private Queue<int> Kolejka;
+        private T Drzewo;
+        private T Pozostałe;
+        private bool FirstRun;
+        public DFS()
+        {
+            Numer = new List<int>();
+            Ponumerowano = 0;
+            Kolejka = new Queue<int>();
+            Drzewo = new T();
+            Pozostałe = new T();
+            FirstRun = true;
+        }
+        public void Clear()
+        {
+            Numer = new List<int>();
+            Ponumerowano = 0;
+            Kolejka = new Queue<int>();
+            Drzewo = new T();
+            Pozostałe = new T();
+            FirstRun = true;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public SearchResult<T> Search(T G, int x)
+        {
+            int v = x;
+            if (FirstRun == true)
+            {
+                for (int i = 0; i < G.VertexCount; i++)
+                {
+                    Numer.Add(0);
+                    Drzewo.AddVertex();
+                    Pozostałe.AddVertex();
+                }
+                Ponumerowano = 1;
+                Numer[v] = 1;
+                FirstRun = false;
+            }
+            List<int> N = G.FindAdjacentEdges(v);
+            foreach (int w in N)
+            {
+                if (Numer[w] == 0)
+                {
+                    Ponumerowano++;
+                    Numer[w] = Ponumerowano;
+                    Drzewo.AddEdge(v, w);
+                    Search(G, w);
+                }
+                else if (Numer[w] < Numer[v])
+                {
+                    Pozostałe.AddEdge(v, w);
+                }
+            }
+            return new SearchResult<T>(Numer, Drzewo, Pozostałe);
+        }
+    }
+}
---
+++ MPGraphsTests/GraphSearch/DFSTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPGraphs.GraphSearch;
+using MPGraphs.GraphStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPGraphs.GraphSearch.Tests
+{
+    [TestClass()]
+    public class DFSTests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            AdjacencyMatrix m = new AdjacencyMatrix();
+            for (int i = 0; i < 7; i++)
+            {
+                m.AddVertex();
+            }
+            m.AddEdge(0, 1);
+            m.AddEdge(0, 4);
+            m.AddEdge(1, 2);
+            m.AddEdge(1, 4);
+            m.AddEdge(1, 5);
+            m.AddEdge(5, 6);
+            m.AddEdge(2, 3);
+            Assert.IsTrue(m.EdgeCount == 7);
+            using (DFS<AdjacencyMatrix> dfs = new DFS<AdjacencyMatrix>())
+            {
+                SearchResult<AdjacencyMatrix> bfsResult = dfs.Search(m, 0);
+                List<int> expectedResult = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+                CollectionAssert.AreEqual(expectedResult, bfsResult.Numer);
+                CollectionAssert.DoesNotContain(bfsResult.Numer, 0);
+                Assert.IsTrue(bfsResult.Drzewo.EdgeCount == 6);
+
+                dfs.Clear();
+
+                bfsResult = dfs.Search(m, 1);
+                expectedResult = new List<int>(new int[] { 2, 1, 4, 5, 3, 6, 7 });
+                CollectionAssert.AreEqual(expectedResult, bfsResult.Numer);
+                CollectionAssert.DoesNotContain(bfsResult.Numer, 0);
+                Assert.IsTrue(bfsResult.Drzewo.EdgeCount == 6);
+            }
+        }
+    }
+}
